a6feaaa7802d310151c4cdb4bc53bcd42df93ac3
mortuusars <mortuusars@gmail.com>
2025-05-20 07:18:26 +0300

    Item model property

--- common/src/main/java/io/github/mortuusars/monobank/MonobankClient.java
+++ common/src/main/java/io/github/mortuusars/monobank/MonobankClient.java
@@ -1,20 +1,24 @@
 package io.github.mortuusars.monobank;
 
+import io.github.mortuusars.monobank.world.block.monobank.MonobankBlockEntity;
+import io.github.mortuusars.monobank.world.block.monobank.component.Lock;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.item.component.CustomData;
 
 public class MonobankClient {
+    @SuppressWarnings("deprecation")
     public static void init() {
         ItemProperties.register(Monobank.Items.MONOBANK.get(), Monobank.resource("locked"), (stack, level, entity, seed) -> {
-            // TODO: implement
-//            if (stack.hasTag() && stack.getTag().contains("BlockEntityTag", CompoundTag.TAG_COMPOUND)) {
-//                CompoundTag blockEntityTag = stack.getTag().getCompound("BlockEntityTag");
-//                if (blockEntityTag.contains(MonobankBlockEntity.LOCK_TAG, CompoundTag.TAG_COMPOUND)) {
-//                    CompoundTag lock = blockEntityTag.getCompound(MonobankBlockEntity.LOCK_TAG);
-//                    return lock.getBoolean(Lock.LOCKED_TAG) ? 1f : 0f;
-//                }
-//            }
-            return 0f;
+            CustomData tag = stack.getOrDefault(DataComponents.BLOCK_ENTITY_DATA, CustomData.EMPTY);
+            if (tag.getUnsafe().contains(MonobankBlockEntity.LOCK_TAG, Tag.TAG_COMPOUND)) {
+                CompoundTag lockTag = tag.getUnsafe().getCompound(MonobankBlockEntity.LOCK_TAG);
+                return lockTag.getBoolean(Lock.LOCKED_TAG) ? 1 : 0;
+            }
+            return 0;
         });
     }
 
